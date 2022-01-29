add0e08171e546f6827e8e8633a9c77bce25cf17
Alexander Kraus <ak@inniti.de>
2022-01-29 12:01:57 +0100

    feat(demo): load environment variables from .env file

--- packages/demo/index.js
+++ packages/demo/index.js
@@ -1,9 +1,23 @@
+require('dotenv').config();
+
 const { createServer } = require('@inniti/middle-server');
 const { useTiming } = require('@envelop/core');
 const Connector1 = require('./connectors/Connector1');
 const Connector2 = require('./connectors/Connector2');
 
-createServer(
-    [Connector1, Connector2],
-    [useTiming()]
-).listen(4000);
+const port = process.env.PORT || 4000;
+
+const envelopPlugins = [];
+const connectors = [Connector1, Connector2];
+
+if (process.env.NODE_ENV === 'development') {
+  envelopPlugins.push(useTiming());
+}
+
+createServer(connectors, envelopPlugins).listen(port, (err) => {
+  if (err) {
+    console.error('An error occured while staring the middle server', err);
+  } else {
+    console.log(`🚀 middle server is running at http://localhost:${port}`);
+  }
+});
